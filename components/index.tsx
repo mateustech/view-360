7c4198545b836a2a1a5fb641ccbeefe527590ca8
mateustech <mateustech686@gmail.com>
2021-06-24 10:13:10 -0300

    fix: refactor tag img

--- components/index.tsx
+++ components/index.tsx
@@ -1,22 +1,33 @@
 import Image from "next/image";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 export const View360 = () => {
     const [deg,setDeg] = useState<number>(1)
-    const handleChange = (event) => {
+    const handleChange = (event: any) => {
         setDeg(event.target.value)
     }
 
     return (
-        <div className="bg-white rounded relative">
-            <input
-                style={{opacity: 0}}
-                className="absolute w-screen h-screen lg:px-40 flex items-center justify-center bg-gray-200"
-                type="range"
-                min="1" max="60"
-                value={deg}
-                onChange={event => handleChange(event)}
-                step="1"/>
-            <img style={{cursor: "col-resize"}} width={1040/2} height={780/2} src={`/frames-360/frames-${deg}.jpeg`}  alt={"frame"}/>
+        <div className="rounded relative w-screen h-screen lg:px-44 flex items-center justify-center bg-gray-200">
+            <div
+                className="relative"
+                style={{width:1040/2, height: 780/2}}
+            >
+                <input
+                    style={{opacity: 0}}
+                    className="absolute w-full h-full z-10"
+                    type="range"
+                    min={1}
+                    max={60}
+                    value={deg}
+                    onChange={event => handleChange(event)}
+                    step="1"/>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    className="w-full"
+                    src={`/frames-360/frames-${deg}.jpeg`}
+                    alt={"frame"}
+                />
+            </div>
         </div>
     )
 }
