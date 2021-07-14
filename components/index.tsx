087c59312b89250f1cb65e8be9310f579c422796
mateustech <mateustech686@gmail.com>
2021-07-14 10:08:42 -0300

    poc

--- components/index.tsx
+++ components/index.tsx
@@ -2,72 +2,112 @@ import Image from "next/image";
 import {useState} from "react";
 export const View360 = () => {
     const [deg,setDeg] = useState<number>(1)
-    const handleChange = (event: any) => {
-        console.log(event.target.value)
-        setDeg(event.target.value)
+    const [degInt,setDegInt] = useState<number>(1)
+    const [int,setInt] = useState<number>(1)
+    const handleChange = (event: any,type: string) => {
+        if(type === "deg"){
+            setDeg(event.target.value)
+        }
+        else if(type === "degInt"){
+            setDegInt(event.target.value)
+        }
+        else if(type === "int"){
+            setInt(event.target.value)
+        }
     }
 
+    const generateArrayByCount = (count: number): number[] => {
+        var array: number[] = []
+        let i:number = 1
+        while(i <= count){
+            array.push(i)
+            i += 1
+        }
+        return array
+    }
     return (
-        <div className="rounded relative w-screen h-screen lg:px-44 flex items-center justify-center bg-gray-200">
+        <div className="rounded relative w-screen h-screen flex items-center justify-center bg-gray-200">
             <div
                 className="relative"
-                style={{width:1040/2, height: 780/2}}
+                style={{width: 1040, height: 780}}
             >
                 <input
                     style={{opacity: 0}}
                     className="absolute w-full h-full z-10"
                     type="range"
                     min={1}
-                    max={59}
+                    max={33}
                     value={deg}
-                    onChange={event => handleChange(event)}
-                    step="2"/>
+                    onChange={event => handleChange(event,"deg")}
+                    step="1"/>
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                     className="w-full"
-                    src={`/frames-360/frames-${deg}.jpeg`}
+                    src={`/frames-eric/frame-${deg}.jpeg`}
+                    // src={`/frames-interior/frame-${deg}.jpeg`}
                     alt={"frame"}
                 />
             </div>
-            {paths.map((item,key) => {
+            {generateArrayByCount(33).map((item,key) => {
                 return (
                     // eslint-disable-next-line @next/next/no-img-element
-                    <img key={key} className="hidden" src={"/frames-360/"+item}/>
+                    <img key={key} className="hidden" src={`/frames-eric/frame-${item}.jpeg`}/>
+                )
+            })}
+            <div
+                className="relative"
+                style={{width:1040, height: 780}}
+            >
+                <input
+                    style={{opacity: 0}}
+                    className="absolute w-full h-full z-10"
+                    type="range"
+                    min={1}
+                    max={9}
+                    value={degInt}
+                    onChange={event => handleChange(event,"degInt")}
+                    step="1"/>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    className="w-full"
+                    src={`/frames-interior/frame-${degInt}.jpeg`}
+                    // src={`/frames-interior/frame-${deg}.jpeg`}
+                    alt={"frame"}
+                />
+            </div>
+            {generateArrayByCount(9).map((item,key) => {
+                return (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img key={key} className="hidden" src={`/frames-interior/frame-${item}.jpeg`}/>
+                )
+            })}
+            <div
+                className="relative"
+                style={{width:1040, height: 780}}
+            >
+                <input
+                    style={{opacity: 0}}
+                    className="absolute w-full h-full z-10"
+                    type="range"
+                    min={1}
+                    max={5}
+                    value={int}
+                    onChange={event => handleChange(event,"int")}
+                    step="1"/>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                    className="w-full"
+                    src={`/interior/frame-${int}.jpeg`}
+                    alt={"frame"}
+                />
+            </div>
+            {generateArrayByCount(5).map((item,key) => {
+                return (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img key={key} className="hidden" src={`/interior/frame-${item}.jpeg`}/>
                 )
             })}
         </div>
     )
 }
 
-const paths = [
-    "frames-1.jpeg",
-    "frames-3.jpeg",
-    "frames-5.jpeg",
-    "frames-7.jpeg",
-    "frames-9.jpeg",
-    "frames-11.jpeg",
-    "frames-13.jpeg",
-    "frames-15.jpeg",
-    "frames-17.jpeg",
-    "frames-19.jpeg",
-    "frames-21.jpeg",
-    "frames-23.jpeg",
-    "frames-25.jpeg",
-    "frames-27.jpeg",
-    "frames-29.jpeg",
-    "frames-31.jpeg",
-    "frames-33.jpeg",
-    "frames-35.jpeg",
-    "frames-37.jpeg",
-    "frames-39.jpeg",
-    "frames-41.jpeg",
-    "frames-43.jpeg",
-    "frames-45.jpeg",
-    "frames-47.jpeg",
-    "frames-49.jpeg",
-    "frames-51.jpeg",
-    "frames-53.jpeg",
-    "frames-55.jpeg",
-    "frames-57.jpeg",
-    "frames-59.jpeg",
-]
